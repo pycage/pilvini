a16239f6be727f47eff78e5d6e96d4ee19dd3c35
Martin Grimme <martin.grimme@gmail.com>
2019-04-25 09:37:35 +0200

    open PDF in a popup

--- res/webshell/extensions/pdf.js
+++ res/webshell/extensions/pdf.js
@@ -2,34 +2,46 @@
 
 (function ()
 {
-    function viewPdf(href)
+    var IFrame = function ()
     {
-        var parts = href.split("/");
-        var name = decodeURI(parts[parts.length - 1]);
-    
-        var page = sh.element(sh.NSPage)
-        .onSwipeBack(function () { page.pop_(); })
-        .header(
-            sh.element(sh.PageHeader).title(name).subtitle("PDF Document")
-            .left(
-                sh.element(sh.IconButton).icon("sh-icon-back")
-                .onClicked(function () { page.pop_(); })
-            )
-        );
-        page.push_();
-    
-        var iframe = $(
+        Object.defineProperties(this, {
+            source: { set: setSource, get: source, enumerable: true }
+        });
+
+        var m_source = "";
+        var m_iframe = $(
             sh.tag("iframe")
-            .style("position", "absolute")
-            .style("top", "-1px")
-            .style("left", "0")
-            .style("width", "100vw")
-            .style("height", "calc(100vh - 3rem)")
-            .attr("src", "/::res/webshell/extensions/pdfjs/web/viewer.html?file=" + encodeURI(href))
+            .style("width", "calc(100vw - 80px)")
+            .style("height", "calc(100vh - 80px)")
             .html()
         );
-    
-        page.get().get().find("> section").append(iframe);
+
+        function setSource(src)
+        {
+            m_iframe.prop("src", src);
+            m_source = src;
+        }
+
+        function source()
+        {
+            return m_source;
+        }
+
+        this.get = function ()
+        {
+            return m_iframe;
+        };
+    };
+
+    function viewPdf(href)
+    {
+        var popup = sh.element(sh.Popup)
+        .add(
+            sh.element(IFrame)
+            .source("/::res/webshell/extensions/pdfjs/web/viewer.html?file=" + encodeURI(href))
+        );
+        
+        popup.show_();
     }
 
     mimeRegistry.register("application/pdf", viewPdf);
