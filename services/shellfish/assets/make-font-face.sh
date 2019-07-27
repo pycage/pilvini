b1709dad9c3fad1dbe09389a4400741e93dd3e9a
Martin Grimme <martin.grimme@gmail.com>
2019-07-27 11:43:48 +0200

    added more icons

--- services/shellfish/assets/make-font-face.sh
+++ services/shellfish/assets/make-font-face.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 
 FONT=`cat "$1" | base64 -w0`
-NAME=`basename "$1" .ttf`
 echo "
 @font-face {
-    font-family: ${NAME};
+    font-family: shellfish-icons;
     src: url(data:font/truetype;charset=utf-8;base64,${FONT}) format('truetype');
     font-weight: normal;
     font-style: normal;
--- services/shellfish/style.js
+++ services/shellfish/style.js
@@ -5,8 +5,9 @@ var styles = [
     __dirname + "/style/effects.css",
     __dirname + "/style/elements.css",
     __dirname + "/style/html.css",
-    __dirname + "/style/icons.css",
+    __dirname + "/style/icon-data.css",
     __dirname + "/style/icon-map.css",
+    __dirname + "/style/icons.css",
     __dirname + "/style/states.css",
     __dirname + "/style/theme.css"
 ];
