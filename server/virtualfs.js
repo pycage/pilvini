bcf1d9ec2d3d188938e36f3f885e9871bbccbe50
Martin Grimme <martin.grimme@gmail.com>
2022-08-20 10:50:34 +0200

    started shellfish-based reimplemtation

---
+++ server/virtualfs.js
@@ -0,0 +1,349 @@
+shRequire(["shellfish/core", "shellfish/core/mime"], (core, mime) =>
+{
+
+    const d = new WeakMap();
+
+    /**
+     * Class for combining multiple filesystem implementations into one.
+     * This allows, e.g., to handle archive files just like directories.
+     * 
+     * The virtual filesystem uses a base filesystem as its entry and a number
+     * of child filesystems for handling selected MIME types.
+     * 
+     * Attach the property `vfsMimeTypes` to the child filesystems to assign
+     * filesystems to MIME types.
+     * 
+     * Each child filesystem must support the `data` property for loading a
+     * file.
+     * 
+     * @extends core.Filesystem
+     * 
+     * @property {core.Filesystem} filesystem - The base file system.
+     */
+    class VirtualFS extends core.Filesystem
+    {
+        constructor()
+        {
+            super();
+            d.set(this, {
+                filesystem: null
+            });
+
+            this.notifyable("filesystem");
+        }
+
+        get filesystem() { return d.get(this).filesystem; }
+        set filesystem(f)
+        {
+            d.get(this).filesystem = f;
+            this.filesystemChanged();
+        }
+
+        /**
+         * Analyzes the given path to find the innermost virtual filesystem, if
+         * any, and splits the path into the external and internal components.
+         * 
+         * @private
+         * 
+         * @param {string} path - The path to analyze.
+         * @returns {object} - The info object. 
+         */
+        analyzePath(path)
+        {
+            const parts = path.split("/");
+            //console.log("ANALYZE " + JSON.stringify(parts));
+            let idx = -1;
+            let mimeType = "";
+            let fs = null;
+
+            for (let i = parts.length - 2; i >= 0; --i)
+            {
+                mimeType = mime.mimeType(parts[i]);
+                fs = this.filesystemFor(mimeType);
+                if (fs)
+                {
+                    idx = i;
+                    break;
+                }
+            }
+
+            if (idx != -1)
+            {
+                const outerPath = parts.slice(0, idx + 1).join("/");
+                const innerPath = parts.slice(idx + 1).join("/");
+
+                return {
+                    fs: fs,
+                    path1: outerPath,
+                    path2: innerPath[0] !== "/" ? "/" + innerPath : innerPath
+                };
+            }
+            else
+            {
+                return {
+                    fs: null,
+                    path1: path,
+                    path2: ""
+                };
+            }
+        }
+
+        /**
+         * Returns the child filesystem to be used for the given MIME type, or
+         * `null` if no child filesystems handles that type.
+         * 
+         * @private
+         * 
+         * @param {string} mimeType - The MIME type.
+         * @returns {core.Filesystem} The filesystem handling the given MIME type.
+         */
+        filesystemFor(mimeType)
+        {
+            return this.children.find(c => c.vfsMimeTypes.includes(mimeType));
+        }
+
+        /**
+         * Reads data from the given path by recursively resolving the stacked
+         * filesystems.
+         * 
+         * @private
+         * 
+         * @param {string} dataPath - The path to read.
+         * @returns {ArrayBuffer} - The data.
+         */
+        async readData(dataPath)
+        {
+            const priv = d.get(this);
+
+            const pathInfo = this.analyzePath(dataPath);
+            if (! pathInfo.fs)
+            {
+                const blob = await priv.filesystem.read(dataPath);
+                return await blob.arrayBuffer();
+            }
+            else
+            {
+                const data = await this.readData(pathInfo.path1);
+                pathInfo.fs.data = data;
+                const blob = await pathInfo.fs.read(pathInfo.path2);
+                return await blob.arrayBuffer();
+            }
+        }
+
+        async fileInfo(path)
+        {
+            //console.log("FILE INFO: " + path);
+
+            const priv = d.get(this);
+
+            if (! priv.filesystem)
+            {
+                return [];
+            }
+
+            const pathInfo = this.analyzePath(path);
+            //console.log(JSON.stringify(pathInfo));
+            if (! pathInfo.fs)
+            {
+                const finfo = await priv.filesystem.fileInfo(path);
+                if (this.filesystemFor(finfo.mimetype))
+                {
+                    console.log("IS A VFS: " + path);
+                    finfo.type = "d";
+                }
+                return finfo;
+            }
+            else
+            {
+                //console.log("READ DATA FOR " + pathInfo.path1);
+                const data = await this.readData(pathInfo.path1);
+                //console.log("SIZE: " + data.length);
+                pathInfo.fs.data = data;
+                const finfo = await pathInfo.fs.fileInfo(pathInfo.path2);
+                if (this.filesystemFor(finfo.mimetype))
+                {
+                    console.log("IS A VFS: " + path);
+                    finfo.type = "d";
+                }
+                console.log(JSON.stringify(finfo));
+                return finfo;
+            }
+        }
+
+        async list(path)
+        {
+            //console.log("LIST " + path);
+
+            const priv = d.get(this);
+
+            if (! priv.filesystem)
+            {
+                return [];
+            }
+
+            const mimeType = mime.mimeType(path);
+            if (this.filesystemFor(mimeType))
+            {
+                path += "/";
+            }
+
+            const pathInfo = this.analyzePath(path);
+            //console.log(JSON.stringify(pathInfo));
+            if (! pathInfo.fs)
+            {
+                return await priv.filesystem.list(path);
+            }
+            else
+            {
+                const data = await this.readData(pathInfo.path1);
+                pathInfo.fs.data = data;
+                const items = await pathInfo.fs.list(pathInfo.path2);
+                items.forEach(item =>
+                {
+                    item.path = this.pathJoin(pathInfo.path1, item.path);
+                    item.dir = this.pathJoin(pathInfo.path1, item.dir);
+                });
+                return items;
+            }
+        }
+
+        async mkdir(path, name)
+        {
+            const priv = d.get(this);
+
+            if (! priv.filesystem)
+            {
+                return;
+            }
+
+            const pathInfo = this.analyzePath(path);
+            if (! pathInfo.fs)
+            {
+                return await priv.filesystem.mkdir(path, name);
+            }
+            else
+            {
+                throw "Not supported";
+                /*
+                const data = await this.readData(pathInfo.path1);
+                pathInfo.fs.data = data;
+                const result = await pathInfo.fs.mkdir(pathInfo.path2, name);
+                this.writeData(pathInfo.path1, pathInfo.fs.data);
+                */
+            }
+        }
+
+        async move(sourcePath, destPath)
+        {
+            const priv = d.get(this);
+
+            if (! priv.filesystem)
+            {
+                return;
+            }
+
+            const pathInfo = this.analyzePath(sourcePath);
+            if (! pathInfo.fs)
+            {
+                return await priv.filesystem.move(sourcePath, destPath);
+            }
+            else
+            {
+                throw "Not supported";
+            }
+        }
+
+        async copy(sourcePath, destPath)
+        {
+            const priv = d.get(this);
+
+            if (! priv.filesystem)
+            {
+                return;
+            }
+
+            const pathInfo = this.analyzePath(sourcePath);
+            if (! pathInfo.fs)
+            {
+                return await priv.filesystem.copy(sourcePath, destPath);
+            }
+            else
+            {
+                throw "Not supported";
+            }
+        }
+
+        async remove(path)
+        {
+            const priv = d.get(this);
+
+            if (! priv.filesystem)
+            {
+                return;
+            }
+
+            const pathInfo = this.analyzePath(path);
+            if (! pathInfo.fs)
+            {
+                return await priv.filesystem.remove(path);
+            }
+            else
+            {
+                throw "Not supported";
+            }
+        }
+
+        async read(path)
+        {
+            console.log("READ " + path);
+
+            const priv = d.get(this);
+
+            if (! priv.filesystem)
+            {
+                return [];
+            }
+
+            const mimeType = mime.mimeType(path);
+            if (this.filesystemFor(mimeType))
+            {
+                path += "/";
+            }
+
+            const pathInfo = this.analyzePath(path);
+            //console.log(JSON.stringify(pathInfo));
+            if (! pathInfo.fs)
+            {
+                return await priv.filesystem.read(path);
+            }
+            else
+            {
+                const data = await this.readData(pathInfo.path1);
+                pathInfo.fs.data = data;
+                return await pathInfo.fs.read(pathInfo.path2);
+            }
+        }
+
+        async write(path, stream)
+        {
+            const priv = d.get(this);
+
+            if (! priv.filesystem)
+            {
+                return;
+            }
+
+            const pathInfo = this.analyzePath(path);
+            if (! pathInfo.fs)
+            {
+                return await priv.filesystem.write(path, stream);
+            }
+            else
+            {
+                throw "Not supported";
+            }
+        }
+    }
+    exports.VirtualFS = VirtualFS;
+
+});
---
+++ server/zipfs.js
@@ -0,0 +1,148 @@
+shRequire(["shellfish/core", "shellfish/core/mime", "./shell/jszip.min.js"], (core, mime, jszip) =>
+{
+    class File
+    {
+        constructor(fileInZip)
+        {
+            this.fileInZip = fileInZip;
+        }
+
+        async arrayBuffer()
+        {
+            return await this.fileInZip.async("nodebuffer");
+        }
+
+        stream(from, to)
+        {
+            if (to !== undefined)
+            {
+                return this.fileInZip.nodeStream();
+            }
+            else
+            {
+                return this.fileInZip.nodeStream();
+            }
+        }
+
+        text()
+        {
+            return this.arrayBuffer();
+        }
+    }
+
+
+    const d = new WeakMap();
+
+    class ZipFS extends core.Filesystem
+    {
+        constructor()
+        {
+            super();
+            d.set(this, {
+                data: null
+            });
+
+            this.notifyable("data");
+        }
+
+        get data() { return d.get(this).data; }
+        set data(dt)
+        {
+            d.get(this).data = dt;
+            this.dataChanged();
+        }
+
+        async fileInfo(path)
+        {
+            if (path === "" || path === "/")
+            {
+                return {
+                    path: "/",
+                    dir: "/",
+                    name: "",
+                    type: "d",
+                    size: 0,
+                    mimetype: "application/x-folder",
+                    ctime: new Date(),
+                    mtime: new Date()
+                };
+            }
+            console.log("VFS FILE INFO: " + path);
+            const files = await this.list(this.dirname(path));
+            const item = files.find(item => item.path === path || "/" + item.path === path);
+            return item;
+        }
+
+        async list(path)
+        {
+            console.log("ZIP LIST: " + path);
+            const zip = await jszip.loadAsync(d.get(this).data);
+
+            console.log(zip.files);
+
+            const seen = new Set();
+            const implicitDirectories = Object.values(zip.files)
+            .map(fileObj => fileObj.name[0] !== "/" ? "/" + fileObj.name : fileObj.name)
+            .filter(p => this.dirname(this.dirname(p)) === path)
+            .map(p => this.dirname(p))
+            .filter(p => p !== "/");
+
+            const items = Object.values(zip.files)
+            .filter(fileObj => ! fileObj.dir)
+            .filter(fileObj => this.dirname("/" + fileObj.name) === path)
+            .map(fileObj =>
+            {
+                seen.add(fileObj.name);
+                return {
+                    path: fileObj.name,
+                    dir: this.dirname("/" + fileObj.name),
+                    name: this.filename(fileObj.name),
+                    type: fileObj.dir ? "d" : "f",
+                    size: fileObj?._data?.uncompressedSize || -1,
+                    mimetype: fileObj.dir ? "application/x-folder" : mime.mimeType(fileObj.name),
+                    ctime: new Date(fileObj.date),
+                    mtime: new Date(fileObj.date)
+                };
+            });
+
+            implicitDirectories.forEach(p =>
+            {
+                if (! seen.has(p))
+                {
+                    items.push({
+                        path: p,
+                        dir: this.dirname(p),
+                        name: this.filename(p),
+                        type: "d",
+                        size: 0,
+                        mimetype: "application/x-folder",
+                        ctime: new Date(),
+                        mtime: new Date()
+                    });
+                    seen.add(p);
+                }
+            });
+
+            return items;
+        }
+
+        async read(path)
+        {
+            console.log("VFS READ " + path);
+            const zip = await jszip.loadAsync(d.get(this).data);
+
+            if (zip.files[path])
+            {
+                return new File(zip.file(path));
+            }
+            else
+            {
+                // strip the leading "/"
+                return new File(zip.file(path.substr(1)));
+            }
+
+        }
+    }
+    exports.ZipFS = ZipFS;
+
+});
