59108c7f5a4eed31946c8e1d0cfd78342c792556
Martin Grimme <martin.grimme@gmail.com>
2022-08-20 11:16:36 +0200

    moved jszip to server directory

--- server/zipfs.js
+++ server/zipfs.js
@@ -1,4 +1,4 @@
-shRequire(["shellfish/core", "shellfish/core/mime", "./jszip.min.js"], (core, mime, jszip) =>
+shRequire(["shellfish/core", "shellfish/core/mime", __dirname + "/jszip.min.js"], (core, mime, jszip) =>
 {
     class File
     {
