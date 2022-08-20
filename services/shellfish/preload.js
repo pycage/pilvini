083178d65c287ddcee8f47a6eb2e3dc8feef9a8b
Martin Grimme <martin.grimme@gmail.com>
2022-08-20 10:56:49 +0200

    removing ancient shellfish implementation

--- services/shellfish/preload.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-require([__dirname + "/style.js",
-         __dirname + "/core/low.js",
-         __dirname + "/core/mid.js",
-         __dirname + "/core/high.js"], function () { });
